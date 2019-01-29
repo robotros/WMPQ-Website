13efe8756d3250674f8a435275de1d3d1b355a1a
Aron Roberts <robotros1@msn.com>
2019-01-29 12:00:57 -0800

    Create Footer.js
    Create React Component for the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+/**
+* React Component to Render footer for WMPQ Gaming
+* @author [Aron Roberts](https://github.com/robotros)
+*/
+class Footer extends Component {
+  /**
+  * Render Component into html
+  * @return {Component} html
+  */
+  render() {
+    return (
+      <footer className='py-5 bg-dark'>
+        <div className='container'>
+          <p className='m-0 text-center text-white'>
+            Copyright &copy; WMPQ Gaming 2018
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
