590c6df000a410b7612a5267797d0c2aac458170
Aron Roberts <robotros1@msn.com>
2019-01-29 11:58:47 -0800

    Create TwitchApi.js
    Setup Twitch API calls

---
+++ src/components/TwitchApi.js
@@ -0,0 +1,37 @@
+// API Endpoint URL
+const api = 'https://api.twitch.tv/helix/';
+
+// Unique Client-ID obtained at https://twitch.tv
+const client = '1qdqw5gt896kmm2mqngp3mepzxod1e';
+const request = {
+  method: 'GET',
+  headers: {
+    'Client-ID': client,
+  },
+};
+
+/*
+* Returns channel information for a list of channels
+*/
+export const getChannels = (list) => {
+  let query='';
+  list.forEach((name, i, arr) => {
+    (i == 0) ? query = query + name.channel :
+      query = query + '&login='+name.channel;
+  });
+  fetch(`${api}users?login=${query}`, request)
+      .then((res) => res.json());
+};
+
+/*
+* Returns Details of live streams from list
+*/
+export const getLive = (list) => {
+  let query='';
+  list.forEach((channel, i, arr) => {
+    (i == 0) ? query = query + channel.id :
+      query = query + '&user_id=' + channel.id;
+  });
+  fetch(`${api}streams?user_id=${query}`, request)
+      .then((res) => res.json());
+};
