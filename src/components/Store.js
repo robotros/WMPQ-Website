5bbff19cc8dbf1d06d18d6d0ac53c1607b4f684a
Aron Roberts <robotros1@msn.com>
2019-02-04 16:50:15 -0800

    feat:store

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import shirt from '../img/FlaashRobot.jpg';
+import hoodie from '../img/QuayHoodie.jpg';
+import vegas from '../img/enviousvegas.jpg';
 
 /**
 * React Component to Render WMPQ Store
@@ -15,7 +18,14 @@ class Store extends Component {
         <h1>WMPQ Merch</h1>
         <p>This page is still under construction.
           We are currently working with several vendors to bring quality products to our members and supporters.
+          Until sourcing is complete you can purchase WMPQ Merchandise from our
+          <a href='https://www.designbyhumans.com/shop/WMPQ/?fbclid=IwAR2qiLo6qbzI0XXQ9VM_ZfBRigGLd42FGWDoEFeQoOFAj-8lfxzdcxmPo6E' > Design By Humans store.</a>
         </p>
+
+        <img src={vegas} className='img-responsive merch' alt='vegas'></img>
+        <img src={hoodie} className='img-responsive merch' alt='hoodie'></img>
+        <img src={shirt} className='img-responsive merch' alt='shirt'></img>
+
       </div>
     );
   }
