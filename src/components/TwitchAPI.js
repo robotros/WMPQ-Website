806ba4ff6e1f3d8745e5f95d13ae1325140d3c06
Aron Roberts <robotros1@msn.com>
2019-04-22 22:59:11 -0700

    convert from fetch to axios

--- src/components/TwitchAPI.js
+++ src/components/TwitchAPI.js
@@ -1,43 +1,68 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 /**
 * Author: [Aron Roberts](github.com/robotros)
 */
+import axios from 'axios';
+import qs from 'qs';
 
 // API Endpoint URL
 const api = 'https://api.twitch.tv/helix/';
 
 // Unique Client-ID obtained at https://twitch.tv
 const client = '1qdqw5gt896kmm2mqngp3mepzxod1e';
-const request = {
-  method: 'GET',
+
+
+const twitchRequest = axios.create({
+  baseURL: api,
+  paramsSerializer: (params) => qs.stringify(params, {arrayFormat: 'repeat'}),
   headers: {
     'client-id': client,
   },
-};
+});
+
 
 /*
 * Returns channel information for a list of channels
 */
 export const getChannels = (list) => {
-  let query='';
+  let query=[];
   list.forEach((name, i, arr) => {
-    (i === 0) ? query = query + name['channel'] :
-      query = query + '&login='+name['channel'];
+    query.push(name.channel);
   });
-  query = api+'users?login='+query;
-  return fetch(query, request)
-      .then((res) => res.json());
+
+  let request = {
+    params: {
+      login: query,
+    },
+  };
+  return twitchRequest.get('users', request)
+      .then((response)=>{
+        return response.data;
+      }).catch((error)=>{
+        console.error('error occured connecting to twitchRequest');
+      });
 };
+
 
 /*
 * Returns Details of live streams from list
 */
 export const getLive = (list) => {
-  let query='';
+  let query=[];
   list.forEach((channel, i, arr) => {
-    (i === 0) ? query = query + channel.id :
-      query = query + '&user_id=' + channel.id;
+    query.push(channel.id);
   });
-  query = api+'streams?user_id='+query;
-  return fetch(query, request)
-      .then((res) => res.json());
+
+  let request = {
+    params: {
+      user_id: query,
+    },
+  };
+
+  return twitchRequest.get('streams', request)
+      .then((response)=>{
+        return response.data;
+      }).catch((error)=>{
+        console.error('error occured connecting to twitchRequest');
+      });
 };
