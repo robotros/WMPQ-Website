c4de16f0e1f9f5100bcd89561a5c837223af5194
Aron Roberts <robotros1@msn.com>
2020-12-31 02:28:18 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {CookiesProvider} from 'react-cookie';
+//import {CookiesProvider} from 'react-cookie';
 import App from './App';
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <CookiesProvider><BrowserRouter><App /></BrowserRouter></CookiesProvider>,
+    <BrowserRouter><App /></BrowserRouter>,
     document.getElementById('root')
 );
 
